0669d4567caea0692ac8584d0d4b4df44157864f
tombensim <tombensim@gmail.com>
2016-10-03 01:17:20 +0300

    prints request data

--- server.js
+++ server.js
@@ -177,7 +177,8 @@ app.post('/webhook', function (req, res) {
 //  var data = JSON.parse(JSON.stringify(req.body));
 
   // Make sure this is a page subscription
-  if (true | data.object == 'page') {
+  if (true) {
+  	console.log(data.toString());	
     // Iterate over each entry
     // There may be multiple if batched
     data.entry.forEach(function(pageEntry) {
